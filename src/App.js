c1da10c08fce90d36a46fc1c39773c73b9550bd2
Jonathan Torres <jonathanlt68@gmail.com>
2023-12-07 13:45:05 -0300

    Update CS

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {FaFacebook, FaSearch, FaAlignJustify } from 'react-icons/fa';
 
 //iniciar: C:\Users\jonat\OneDrive\Documentos\GitHub\DankiCode-FacebookClone\Facebook-Clone\facebookclone>npm start ou no proprio terminal 'npm start'.
 
+//aula terminada em 24:50
+
 function App() {
   return (
     <div className="App">
@@ -12,25 +14,19 @@ function App() {
           <div className='logo-fb'>
             <FaFacebook/>
           </div>
-
-          <div className='search-fb'>
-            <FaSearch/>
+            <div className='search-fb'>
+              <FaSearch/>
+            </div>
+            <div className='menu-fb'>
+              <FaAlignJustify/>
+            </div>
           </div>
-
-          <div className='menu-fb'>
-            <FaAlignJustify/>
+          
+          <div className='headerRigth'>
+                <div className='plus-btn'>
+                    +
+                </div>
           </div>
-
-        </div>
-
-
-      <div className='headerRigth'>
-        <div className='plus-btn'>
-            +
-        </div>
-      </div>
-
-
       </div>      
     </div>
   );
