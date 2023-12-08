e5d91ea43b87c777485cf0dce52bde5bf7e7e489
Jonathan Torres <jonathanlt68@gmail.com>
2023-12-08 10:58:14 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-
+import danki_bg from './resources/danki_bg.webp';
 import Header from './Header';
 
 //iniciar: C:\Users\jonat\OneDrive\Documentos\GitHub\DankiCode-FacebookClone\Facebook-Clone\facebookclone>npm start ou no proprio terminal 'npm start'.
@@ -11,19 +11,27 @@ function App() {
     <div className="App">
       <Header />
         <div className='stories'>
-            <div className='storiesCard' style={{backgroundImage: 'url(${danki_bg})'}}>
+            <div className='storiesCard' style={{backgroundImage: `url(${danki_bg})`}}>
               <p>Jonathan Torres</p>
             </div>
-            <div className='storiesCard' style={{backgroundImage: 'url(${danki_bg})'}}>
+        </div>
+        <div className='stories'>
+            <div className='storiesCard' style={{backgroundImage: `url(${danki_bg})`}}>
               <p>Jonathan Torres</p>
             </div>
-            <div className='storiesCard' style={{backgroundImage: 'url(${danki_bg})'}}>
+        </div>
+        <div className='stories'>
+            <div className='storiesCard' style={{backgroundImage: `url(${danki_bg})`}}>
               <p>Jonathan Torres</p>
             </div>
-            <div className='storiesCard' style={{backgroundImage: 'url(${danki_bg})'}}>
+        </div>
+        <div className='stories'>
+            <div className='storiesCard' style={{backgroundImage: `url(${danki_bg})`}}>
               <p>Jonathan Torres</p>
             </div>
-            <div className='storiesCard' style={{backgroundImage: 'url(${danki_bg})'}}>
+        </div>
+        <div className='stories'>
+            <div className='storiesCard' style={{backgroundImage: `url(${danki_bg})`}}>
               <p>Jonathan Torres</p>
             </div>
         </div>
