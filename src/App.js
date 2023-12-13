6b95feb952d2196792db8ea59c389c212b818d23
Jonathan Torres <jonathanlt68@gmail.com>
2023-12-13 14:39:04 -0300

    Update
    Criando arquivo stories

--- src/App.js
+++ src/App.js
@@ -1,38 +1,24 @@
 import './App.css';
 import danki_bg from './resources/danki_bg.jpg';
 import agumon_bg from './resources/agumon_bg.png';
+import Stories from './Stories';
 import Header from './Header';
 
 //iniciar: C:\Users\jonat\OneDrive\Documentos\GitHub\DankiCode-FacebookClone\Facebook-Clone\facebookclone>npm start ou no proprio terminal 'npm start'.
 
-//aula terminada em 41:00
+//aula terminada em 49:00
 
 function App() {
   return (
     <div className="App">
       <Header />
-        <div className='stories'>
-            <div className='storiesCard' style={{backgroundImage: `url(${agumon_bg})`}}>
-              <p>Jonathan Torres</p>
-            </div>
-                    
-            <div className='storiesCard' style={{backgroundImage: `url(${danki_bg})`}}>
-              <p>Jonathan Torres</p>
-            </div>
-
-            <div className='storiesCard' style={{backgroundImage: `url(${agumon_bg})`}}>
-              <p>Jonathan Torres</p>
-            </div>
-
-            <div className='storiesCard' style={{backgroundImage: `url(${danki_bg})`}}>
-              <p>Jonathan Torres</p>
-            </div>
-
-            <div className='storiesCard' style={{backgroundImage: `url(${agumon_bg})`}}>
-              <p>Jonathan Torres</p>
-            </div>
-
+      <Stories/>
+      <div className='feed'>
+        <div className='feedForm'>
+            <img src={agumon_bg} />
+            <input type='text'   />
         </div>
+      </div>
     </div>  
   );
 }
